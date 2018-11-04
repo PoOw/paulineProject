d4edb1025fcb1226803cf80f0b383b18d47987d3
Pauline Huguenel <pjhuguen@stud.ntnu.no>
2018-11-04 20:21:30 +0100

    Display number of result and first song, hide loader

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,20 +1,57 @@
 // @flow
 import * as React from 'react';
 
-import { FormattedMessage } from 'react-intl';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import StyledLoader from './Home.style';
+import StyledLoader, {StyledTitle} from './Home.style';
 
+const apiToken = 'BQDkyx2b7I9QUM0yuMjz_gVlF83Iihfn7TuVPMC9P-cB1zLApCcLuvJAwuh7KIE4cB_moITfebgO-Lu5GvXarH8ovy58W5ZHhZgrbYW0nHWDAfglq_yrTJzYuA1Q5QA9u73kLnx8U9wcRMfp_TPEg-JdXvQ'
 
-const Home = () => (
-  <React.Fragment>
-    <StyledLoader>
-      <CircularProgress/>
-    </StyledLoader>
-  </React.Fragment>
-);
+class Home extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      songsLoaded: false,
+      data: null
+    }
+  }
+
+  render () {
+    if (this.state.songsLoaded) {
+      const {total} = this.state.data;
+      const {name} = this.state.data.items[0].track;
+      return (
+        <React.Fragment>
+          <StyledTitle>
+            Il y a {total} résultats.<br/>
+            Le titre de la première chanson est : {name}.
+          </StyledTitle>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <StyledLoader>
+            <CircularProgress/>
+          </StyledLoader>
+        </React.Fragment>
+      );
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.spotify.com/v1/me/tracks', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + apiToken,
+      },
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log("Réponse reçue ! Voilà ce que j'ai reçu : ", data);
+      this.setState({songsLoaded: true, data: data});
+    })
+  }
+}
 
 export default Home;
--- client/src/pages/Home/Home.style.js
+++ client/src/pages/Home/Home.style.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 const StyledLoader = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledTitle = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
